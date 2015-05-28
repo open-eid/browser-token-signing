0fafc7f9d28a3bf8633976af9cdd9f549c599adc
allan.juhanson <allan.juhanson@nortal.com>
2015-05-28 17:57:46 +0300

    IB-2782 switched MD5 Hash calculation to use CAPI instead of openssl, removed openssl dependency from project as it is not required anymore

--- ie/EstEIDHelper.h
+++ ie/EstEIDHelper.h
@@ -21,7 +21,8 @@
 
 #pragma once
 
-#include <openssl/md5.h>
+#include <windows.h>
+#include <Wincrypt.h>
 #include <string>
 
 #define MD5_HASH_LEN 16
@@ -35,19 +36,39 @@ public:
 	~CEstEIDHelper(void);
 
 	static char* calculateMD5Hash(char* certificate) {	
-		unsigned char certMD5[MD5_DIGEST_LENGTH];
-		char result[2 * MD5_DIGEST_LENGTH + 1] = "";
-		char chunk[3];
-		int i;
-
-		MD5((unsigned char*)certificate, strlen(certificate), certMD5);
-		
-		for(i = 0; i < MD5_DIGEST_LENGTH; i++){
-			sprintf_s(chunk, 3, "%02X", certMD5[i]);
-			strcat_s(result, MD5_DIGEST_LENGTH * 2 + 1, chunk);
+		HCRYPTPROV cryptProv;
+		if (!CryptAcquireContext(&cryptProv, NULL, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
+			return NULL;
 		}
-		
-		//EstEID_log("cert hash as HEX string: %s", result);	
+
+		HCRYPTHASH cryptHash;
+		if (!CryptCreateHash(cryptProv, CALG_MD5, 0, 0, &cryptHash)) {
+			CryptReleaseContext(cryptProv, 0);
+			return NULL;
+		}
+
+		if (!CryptHashData(cryptHash, (BYTE*)certificate, strlen(certificate), 0)) {
+			CryptReleaseContext(cryptProv, 0);
+			CryptDestroyHash(cryptHash);
+			return NULL;
+		}
+
+		DWORD cbHash = MD5_HASH_LEN;
+		BYTE hash[MD5_HASH_LEN];
+		if (!CryptGetHashParam(cryptHash, HP_HASHVAL, hash, &cbHash, 0)) {
+			CryptReleaseContext(cryptProv, 0);
+			CryptDestroyHash(cryptHash);
+			return NULL;
+		}
+
+		char *hex = "0123456789abcdef";
+		char result[2 * MD5_HASH_LEN + 1] = "";
+		for (int i = 0; i < cbHash; i++) {
+			result[i * 2] = hex[hash[i] >> 4];
+			result[(i * 2) + 1] = hex[hash[i] & 0xF];
+		}
+		CryptReleaseContext(cryptProv, 0);
+		CryptDestroyHash(cryptHash);
 		return _strdup(result);
 	};
 
