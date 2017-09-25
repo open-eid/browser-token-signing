35782b0e28484d1fc249da3f29f007f6abc211f0
Raul Metsma <raul@metsma.ee>
2017-09-25 23:24:51 +0300

    Remove legacy code
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- uitests/blockPIN.cmd
+++
@@ -1,3 +0,0 @@
-pkcs15-tool --reader %1 --change-pin --auth-id %2 --pin 00000 --new-pin 00900 2 > NUL
-pkcs15-tool --reader %1 --change-pin --auth-id %2 --pin 00000 --new-pin 00900 2 > NUL
-pkcs15-tool --reader %1 --change-pin --auth-id %2 --pin 00000 --new-pin 00900 2 > NUL
--- uitests/kill_webdriver_locker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RETPID=`lsof -t -i TCP:7054`
-if [ "$RETPID" != "" ] ; then
-	echo "Killing WebDriver locker with PID $RETPID"
-	kill -9 $RETPID 
-fi
