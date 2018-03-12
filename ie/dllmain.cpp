e4a991c6317e868cfa957dee8b4d46c192a6f813
Raul Metsma <raul@metsma.ee>
2018-03-12 09:59:47 +0200

    Enable precompiled headers to speedup compile times
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- ie/dllmain.cpp
+++ ie/dllmain.cpp
@@ -1,18 +1,26 @@
+/*
+* Estonian ID card plugin for web browsers
+*
+* Copyright (C) 2010-2011 Codeborne <info@codeborne.com>
+*
+* This is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*/
+
 #include "stdafx.h"
 #include "esteidpluginie_i.h"
 #include "dllmain.h"
 
 CesteidpluginieModule _AtlModule;
-
-//// before MFC support 
-// DLL Entry Point
-/*
-extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
-{
-	if (dwReason == DLL_PROCESS_ATTACH) {
-	DisableThreadLibraryCalls(hInstance);
-    }
-	return _AtlModule.DllMain(dwReason, lpReserved); 
-}
-*/
-//// before MFC support end
--- ie/stdafx.h
+++ ie/stdafx.h
@@ -1,11 +1,47 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently,
-// but are changed infrequently
+/*
+* Chrome Token Signing Native Host
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #pragma once
 
 #include "targetver.h"
-#include <afxrich.h>
 #include "resource.h"
+#include <afxrich.h>
 #include <atlctl.h>
+#include <comutil.h>
  
+#include <Windows.h>
+#include <cryptuiapi.h>
+#include <Knownfolders.h>
+#include <ncrypt.h>
+#include <Shlobj.h>
+#include <Shlwapi.h>
+#include <WinCrypt.h>
+
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <cstring>
+#include <cstdarg>
+#include <fcntl.h>
+#include <io.h>
+#include <stdio.h>
+#include <time.h>
