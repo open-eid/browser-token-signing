ef6f353939d641e71104e2380c9bf23ba2bde28b
Raul Metsma <raul@metsma.ee>
2020-01-16 14:10:07 +0200

    Add G2 Latvia card ATR
    IB-6231
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- common/PKCS11Path.cpp
+++ common/PKCS11Path.cpp
@@ -39,30 +39,30 @@
 #include <map>
 
 std::vector<std::string> PKCS11Path::atrList() {
-	SCARDCONTEXT hContext = 0;
-	std::vector<std::string> result;
-	LONG err = SCardEstablishContext(SCARD_SCOPE_USER, nullptr, nullptr, &hContext);
-	if (err != SCARD_S_SUCCESS) {
-		_log("SCardEstablishContext ERROR: %x", err);
-		return result;
-	}
+    SCARDCONTEXT hContext = 0;
+    std::vector<std::string> result;
+    LONG err = SCardEstablishContext(SCARD_SCOPE_USER, nullptr, nullptr, &hContext);
+    if (err != SCARD_S_SUCCESS) {
+        _log("SCardEstablishContext ERROR: %x", err);
+        return result;
+    }
 
-	DWORD size = 0;
-	err = SCardListReaders(hContext, nullptr, nullptr, &size);
-	if (err != SCARD_S_SUCCESS || !size) {
-		_log("SCardListReaders || !size ERROR: %x", err);
-		SCardReleaseContext(hContext);
-		return result;
-	}
+    DWORD size = 0;
+    err = SCardListReaders(hContext, nullptr, nullptr, &size);
+    if (err != SCARD_S_SUCCESS || !size) {
+        _log("SCardListReaders || !size ERROR: %x", err);
+        SCardReleaseContext(hContext);
+        return result;
+    }
 
-	std::string readers(size, 0);
-	err = SCardListReaders(hContext, nullptr, &readers[0], &size);
-	readers.resize(size);
-	if (err != SCARD_S_SUCCESS) {
-		_log("SCardListReaders ERROR: %x", err);
-		SCardReleaseContext(hContext);
-		return result;
-	}
+    std::string readers(size, 0);
+    err = SCardListReaders(hContext, nullptr, &readers[0], &size);
+    readers.resize(size);
+    if (err != SCARD_S_SUCCESS) {
+        _log("SCardListReaders ERROR: %x", err);
+        SCardReleaseContext(hContext);
+        return result;
+    }
 
     std::vector<SCARD_READERSTATE> list;
     for (const char *name = readers.c_str(); *name; name += strlen(name) + 1) {
@@ -107,27 +107,32 @@ PKCS11Path::Params PKCS11Path::getPkcs11ModulePath() {
 #elif defined __APPLE__
     static const std::string openscPath("/Library/OpenSC/lib/opensc-pkcs11.so");
     static const std::string estPath = openscPath;
-    static const std::string latPath("/Library/latvia-eid/lib/otlv-pkcs11.so");
+    static const std::string latPath("/Library/latvia-eid/lib/eidlv-pkcs11.bundle/Contents/MacOS/eidlv-pkcs11");
     static const std::string finPath("/Library/mPolluxDigiSign/libcryptoki.dylib");
     static const std::string lit1Path("/Library/Security/tokend/CCSuite.tokend/Contents/Frameworks/libccpkip11.dylib");
     static const std::string lit2Path("/Library/PWPW-Card/pwpw-card-pkcs11.so");
     static const std::string litPath = access(lit1Path.c_str(), F_OK) == 0 ? lit1Path : lit2Path;
     static const std::string belPath("/usr/local/lib/beid-pkcs11.bundle/Contents/MacOS/libbeidpkcs11.dylib");
     static const std::string eTokenPath("/Library/Frameworks/eToken.framework/Versions/Current/libeToken.dylib");
+    static const std::string acsPath("/usr/local/lib/libacos5pkcs11.dylib");
+    static const std::string akisPath("/usr/local/lib/libakisp11.dylib");
 #else
     static const std::string openscPath("opensc-pkcs11.so");
     static const std::string estPath = openscPath;
-    static const std::string latPath("otlv-pkcs11.so");
+    static const std::string latPath("/opt/latvia-eid/lib/eidlv-pkcs11.so");
     static const std::string finPath("libcryptoki.so");
     static const std::string lit1Path("/usr/lib/ccs/libccpkip11.so");
     static const std::string lit2Path("pwpw-card-pkcs11.so");
     static const std::string litPath = access(lit1Path.c_str(), F_OK) == 0 ? lit1Path : lit2Path;
     static const std::string belPath("libbeidpkcs11.so.0");
     static const std::string eTokenPath("/usr/lib/libeTPkcs11.so");
+    static const std::string acsPath("/lib/libacospkcs11.so");
+    static const std::string akisPath("/usr/lib/libpkcs11wrapper.so");
 #endif
     static const std::map<std::string, Params> m = {
 #ifdef _WIN32
         {"3BFD1800008031FE4553434536302D43443134352D46CD", {"C:\\Windows\\System32\\aetpkss1.dll", "PIN", "PIN"}},
+        {"3B9F968131FE45806755454B41451293318073B3A1806B", {"C:\\Windows\\System32\\akisp11.dll", "PIN", "PIN"}},
 #else
         {"3BFE1800008031FE454573744549442076657220312E30A8", {estPath, "PIN1", "PIN2"}}, //ESTEID_V3_COLD_DEV1_ATR
         {"3BFE1800008031FE45803180664090A4561B168301900086", {estPath, "PIN1", "PIN2"}}, //ESTEID_V3_WARM_DEV1_ATR
@@ -137,6 +142,7 @@ PKCS11Path::Params PKCS11Path::getPkcs11ModulePath() {
         {"3BDB960080B1FE451F830012233F536549440F9000F1", {estPath, "PIN1", "PIN2"}}, // IDEMIA 2018
 
         {"3BDD18008131FE45904C41545649412D65494490008C", {latPath, "PIN1", "PIN2"}},
+        {"3BDB960080B1FE451F830012428F536549440F900020", {latPath, "PIN1", "PIN2"}},
 
         {"3B7B940000806212515646696E454944", {finPath, "PIN1", "PIN2"}},
         {"3B7F9600008031B865B0850300EF1200F6829000", {finPath, "PIN1", "PIN2"}},
@@ -145,6 +151,8 @@ PKCS11Path::Params PKCS11Path::getPkcs11ModulePath() {
 
         {"3BD5180081313A7D8073C8211030", {eTokenPath, "PIN", "PIN"}},
         {"3BD518008131FE7D8073C82110F4", {eTokenPath, "PIN", "PIN"}},
+        {"3BBE9600004105200000000000000000009000", {acsPath, "PIN", "PIN" }},
+        {"3B9F968131FE45806755454B41451293318073B3A1806B", {akisPath, "PIN", "PIN"}},
 #endif
         {"3BF81300008131FE45536D617274417070F8", {litPath, "PIN", "PIN"}},
         {"3B7D94000080318065B08311C0A983009000", {litPath, "PIN", "PIN"}},
@@ -155,8 +163,8 @@ PKCS11Path::Params PKCS11Path::getPkcs11ModulePath() {
 
     const std::vector<std::string> list = atrList();
     for (const std::string &atr : list) {
-        auto it = m.find(atr);
-        if (it != m.end())
+        const auto it = m.find(atr);
+        if (it != m.cend())
             return it->second;
     }
 #ifdef _WIN32
